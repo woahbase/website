5e7a811182c404eb6d6197046a6516917ca21404
lapnapra <34398784+lapnapra@users.noreply.github.com>
2018-02-19 14:04:53 +0530

    fix dropdowns, sort by name, update data

--- src/components/ImagesList.vue
+++ src/components/ImagesList.vue
@@ -26,6 +26,7 @@
                         <div class="">
                             <div class="select">
                                 <select v-model="sort" class="has-text-grey">
+                                    <option value="+repo.name"> &nbsp; &#x2193; Name</option>
 
                                     <option disabled selected="true" class="has-text-dark">Github</option>
                                     <option value="-repo.created_at"> &nbsp; &#x2193; Repo Created</option>
@@ -41,10 +42,10 @@
                                     <!-- <option value="+repo.watchers_count"> &#38;nbsp; &#38;#x2191; Watchers</option> -->
 
                                     <option disabled selected="true" class="has-text-dark">Docker Hub</option>
-                                    <option value="-repo.docker.last_updated"> &nbsp; &#x2193; Image Pushed</option>
-                                    <option value="+repo.docker.last_updated"> &nbsp; &#x2191; Image Pushed</option>
-                                    <option value="+repo.docker.pull_count"> &nbsp; &#x2193; Image Pulled</option>
-                                    <option value="-repo.docker.pull_count"> &nbsp; &#x2191; Image Pulled</option>
+                                    <option value="-repo.docker.last_updated"> &nbsp; &#x2193; Last Pushed</option>
+                                    <option value="+repo.docker.last_updated"> &nbsp; &#x2191; Last Pushed</option>
+                                    <option value="+repo.docker.pull_count"> &nbsp; &#x2193; Image Pulls</option>
+                                    <option value="-repo.docker.pull_count"> &nbsp; &#x2191; Image Pulls</option>
 
                                     <!-- <option value="&#45;repo.docker.star_count"> &#38;nbsp; &#38;#x2193; Stars</option> -->
                                     <!-- <option value="+repo.docker.star_count"> &#38;nbsp; &#38;#x2191; Stars</option> -->
@@ -167,28 +168,29 @@ export default {
         repositories () {
             return this.$store.state.repositories.sort((a, b) => {
                     return {
-                        // '+repo.id' : (a, b) => { return b.id - a.id }, // same as created at
-                        // '-repo.id' : (a, b) => { return a.id - b.id },
-                        '+repo.created_at' : (a, b) => { return new Date(a.github.created_at).getTime() - new Date(b.github.created_at).getTime() },
-                        '-repo.created_at' : (a, b) => { return new Date(b.github.created_at).getTime() - new Date(a.github.created_at).getTime() },
-                        '+repo.updated_at' : (a, b) => { return new Date(a.github.updated_at).getTime() - new Date(b.github.updated_at).getTime() },
-                        '-repo.updated_at' : (a, b) => { return new Date(b.github.updated_at).getTime() - new Date(a.github.updated_at).getTime() },
-                        '+repo.pushed_at' : (a, b) => { return new Date(a.github.pushed_at).getTime() - new Date(b.github.pushed_at).getTime() },
-                        '-repo.pushed_at' : (a, b) => { return new Date(b.github.pushed_at).getTime() - new Date(a.github.pushed_at).getTime() },
-                        '+repo.stargazers_count' : (a, b) => { return b.github.stargazers_count - a.github.stargazers_count },
-                        '-repo.stargazers_count' : (a, b) => { return a.github.stargazers_count - b.github.stargazers_count },
-                        '+repo.watchers_count' : (a, b) => { return b.github.watchers_count - a.github.watchers_count },
-                        '-repo.watchers_count' : (a, b) => { return a.github.watchers_count - b.github.watchers_count },
+                        '+repo.name'                : (a, b) => { return a.name.localeCompare(b.name)},
+                        // '+repo.id'               : (a, b) => { return b.id - a.id }, // same as created at
+                        // '-repo.id'               : (a, b) => { return a.id - b.id },
+                        '+repo.created_at'          : (a, b) => { return new Date(a.github.created_at).getTime() - new Date(b.github.created_at).getTime() },
+                        '-repo.created_at'          : (a, b) => { return new Date(b.github.created_at).getTime() - new Date(a.github.created_at).getTime() },
+                        '+repo.updated_at'          : (a, b) => { return new Date(a.github.updated_at).getTime() - new Date(b.github.updated_at).getTime() },
+                        '-repo.updated_at'          : (a, b) => { return new Date(b.github.updated_at).getTime() - new Date(a.github.updated_at).getTime() },
+                        '+repo.pushed_at'           : (a, b) => { return new Date(a.github.pushed_at).getTime() - new Date(b.github.pushed_at).getTime() },
+                        '-repo.pushed_at'           : (a, b) => { return new Date(b.github.pushed_at).getTime() - new Date(a.github.pushed_at).getTime() },
+                        '+repo.stargazers_count'    : (a, b) => { return b.github.stargazers_count - a.github.stargazers_count },
+                        '-repo.stargazers_count'    : (a, b) => { return a.github.stargazers_count - b.github.stargazers_count },
+                        '+repo.watchers_count'      : (a, b) => { return b.github.watchers_count - a.github.watchers_count },
+                        '-repo.watchers_count'      : (a, b) => { return a.github.watchers_count - b.github.watchers_count },
 
                         '+repo.docker.last_updated' : (a, b) => { return new Date(a.dockerhub.last_updated).getTime() - new Date(b.dockerhub.last_updated).getTime() },
                         '-repo.docker.last_updated' : (a, b) => { return new Date(b.dockerhub.last_updated).getTime() - new Date(a.dockerhub.last_updated).getTime() },
-                        '+repo.docker.pull_count' : (a, b) => { return b.dockerhub.pull_count - a.dockerhub.pull_count },
-                        '-repo.docker.pull_count' : (a, b) => { return a.dockerhub.pull_count - b.dockerhub.pull_count },
-                        '+repo.docker.star_count' : (a, b) => { return b.dockerhub.star_count - a.dockerhub.star_count },
-                        '-repo.docker.star_count' : (a, b) => { return a.dockerhub.star_count - b.dockerhub.star_count },
+                        '+repo.docker.pull_count'   : (a, b) => { return b.dockerhub.pull_count - a.dockerhub.pull_count },
+                        '-repo.docker.pull_count'   : (a, b) => { return a.dockerhub.pull_count - b.dockerhub.pull_count },
+                        '+repo.docker.star_count'   : (a, b) => { return b.dockerhub.star_count - a.dockerhub.star_count },
+                        '-repo.docker.star_count'   : (a, b) => { return a.dockerhub.star_count - b.dockerhub.star_count },
 
-                        '+repo.image.size' : (a, b) => { return b.microbadger.DownloadSize - a.microbadger.DownloadSize },
-                        '-repo.image.size' : (a, b) => { return a.microbadger.DownloadSize - b.microbadger.DownloadSize },
+                        '+repo.image.size'          : (a, b) => { return b.microbadger.DownloadSize - a.microbadger.DownloadSize },
+                        '-repo.image.size'          : (a, b) => { return a.microbadger.DownloadSize - b.microbadger.DownloadSize },
                     }[this.sort](a, b);
             }).filter(v=>
                 v.name.includes(this.query.toLowerCase())
